8252f45de01978c7bb7e184a8b681fe88e6394c8
Jackson Taylor <josephjacksontaylor@gmail.com>
2018-10-16 16:13:19 -0600

    finished all the problems, SpecRunner green

--- practice.js
+++ practice.js
@@ -28,6 +28,9 @@
 */
 
 // Code Here 
+function first (arr, cb) {
+  cb(arr[0]);
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,6 +51,9 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last (arr, cb) {
+  cb(arr[arr.length-1]);
+};
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,6 +72,9 @@ last(names, function(lastName){
 */
 
 //Code Here
+function multiply (num1, num2, cb) {
+  cb(num1*num2);
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -85,6 +94,15 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains (arr, name, cb) {
+  for (let i=0;i<arr.length;i++) {
+    if (arr[i]===name) {
+      cb(true);
+    } else {
+      cb(false);
+    }
+  }
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -106,6 +124,15 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
+function uniq (array, cb) {
+  var arr = [];
+  for (var i=0;i<array.length;i++) {
+      if(arr.indexOf(array[i])===-1) {
+        arr.push(array[i]);
+      } 
+    }
+    cb(arr);
+  }
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -123,6 +150,11 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
+function each (arr, cb) {
+  for (var i=0;i<arr.length;i++) {
+    cb(arr[i],i);
+  }
+}
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -140,6 +172,13 @@ each(names, function(item, indice){
 */
 
 // Code here
+function getUserById (arr, id, cb) {
+  for (var i=0;i<arr.length;i++) {
+    if(arr[i].id===id) {
+      cb(arr[i]);
+    }
+  }
+}
 
 // Do not edit the code below.
 var users = [
